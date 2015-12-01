bd5fce7ee49c4884acc8716cdf164cf980966b2a
Carlos M. Martinez <cmm@cagnazzo.me>
2015-12-01 15:34:59 -0300

    added run.sh wrapper

---
+++ bin/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Python wrapper, (c) carlos@xt6.us, 20151201
+
+PYTHON="/usr/bin/python"
+export SRCHOME=$(pwd)
+
+####
+export PYTHONPATH="$SRCHOME/lib:$SRCHOME/bin:$PYTHONPATH"
+
+$PYTHON $*
